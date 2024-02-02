7f1740cab3b26442dfdaacd6e1198b8433e4d127
Erick-Andrade15 <erickandrade15a@gmail.com>
2024-02-02 12:09:02 -0500

    Codigo para hacer un http get de los datos del json y remplazar en la variable data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'linkfree-clone';
+  data: any;
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.http.get('assets/data.json').subscribe((data) => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
+
+
 }
